93a3847fea0d62d0f5c90dbd21556515589accb4
MatheusBrandalise <matheusjosebrandalise@gmail.com>
2022-01-10 15:05:18 -0300

    Segundo exercício finalizado

--- src/Movies/Movie.java
+++ src/Movies/Movie.java
@@ -14,8 +14,39 @@ public class Movie {
 		this.link = link;
 	}
 
-
 	Movie() {
 		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	public String getMovieGenre() {
+		return movieGenre;
+	}
+
+	public void setMovieGenre(String movieGenre) {
+		this.movieGenre = movieGenre;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
 	}
 }
---
+++ src/Users/User.java
@@ -0,0 +1,12 @@
+package Users;
+
+import java.sql.Date;
+
+public class User {
+	
+	String firstName;
+	String lastName;
+	String completeAdress;
+	Date bornDate;
+
+}
