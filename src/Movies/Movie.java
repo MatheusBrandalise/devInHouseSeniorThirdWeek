525788598d68c0485ba974c696c074be7ed31cd4
MatheusBrandalise <matheusjosebrandalise@gmail.com>
2022-01-10 14:53:31 -0300

    Primeiro exercício finalizado

---
+++ src/Movies/Movie.java
@@ -0,0 +1,21 @@
+package Movies;
+
+public class Movie {
+
+	String name;
+	String synopsis;
+	String movieGenre;
+	String link;
+	
+	Movie(String name, String synopsis, String movieGenre, String link) {
+		this.name = name;
+		this.synopsis = synopsis;
+		this.movieGenre = movieGenre;
+		this.link = link;
+	}
+
+
+	Movie() {
+		
+	}
+}
